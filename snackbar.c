10a5fc7b21d0a07887ec68c3b538d9facd46a21b
StephanRumigny <142924223+StephanRumigny@users.noreply.github.com>
2023-12-04 08:49:39 -0800

    snackbar.c

--- snackbar.c
+++ snackbar.c
@@ -1,1 +1,86 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
 
+// Number of menu items
+// Adjust this value (10) to the number of items input below
+#define NUM_ITEMS 10
+
+// Menu items have item name and price
+typedef struct
+{
+    string item;
+    float price;
+} menu_item;
+
+// Array of menu items
+menu_item menu[NUM_ITEMS];
+
+// Add items to the menu
+void add_items(void);
+
+// Calculate total cost
+float get_cost(string item);
+
+int main(void)
+{
+    add_items();
+
+    printf("\nWelcome to Beach Burger Shack!\n");
+    printf("Choose from the following menu to order. Press enter when done.\n\n");
+
+    for (int i = 0; i < NUM_ITEMS; i++)
+    {
+        printf("%s: $%.2f\n", menu[i].item, menu[i].price);
+    }
+    printf("\n");
+
+    float total = 0;
+    while (true)
+    {
+        string item = get_string("Enter a food item: ");
+        if (strlen(item) == 0)
+        {
+            printf("\n");
+            break;
+        }
+
+        total += get_cost(item);
+    }
+
+    printf("Your total cost is: $%.2f\n", total);
+}
+
+// Add at least the first four items to the menu array
+void add_items(void)
+{
+    menu[0].item = "Burger";
+    menu[0].price = 9.5;
+
+    menu[1].item = "Vegan Burger";
+    menu[1].price = 11;
+
+    menu[2].item = "Hot Dog";
+    menu[2].price = 5;
+
+    menu[3].item = "Cheese Dog";
+    menu[3].price = 7;
+}
+
+// Search through the menu array to find an item's cost
+float get_cost(string item)
+{
+    for (int i = 0; i < NUM_ITEMS; i++)
+    {
+        // Case-insensitive string comparison
+        if (strcasecmp(item, menu[i].item) == 0)
+        {
+            return menu[i].price;
+        }
+    }
+
+    printf("ERROR! try again.\n");
+    return 0.0;
+}
